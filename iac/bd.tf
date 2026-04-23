92366688e2b69d24f4c06fc9bb2f4752476adddf
marlonrm18 <marlonrm6@gmail.com>
2026-04-23 21:56:36 +0000

    fix(iac): uso de la interpolacion para la contraseña de postgreSQL

--- iac/bd.tf
+++ iac/bd.tf
@@ -6,4 +6,8 @@ resource "docker_container" "bd" {
         internal = "5432"
         external = var.bd_port[terraform.workspace]
     }
+
+    env = [
+        "POSTGRES_PASSWORD=${var.bd_password[terraform.workspace]}"
+    ]
 }
